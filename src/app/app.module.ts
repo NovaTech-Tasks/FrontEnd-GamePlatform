b147a1615ba1ae9f3887b417e9bb0e04304dcca7
ThanuraSilva <thanurasilva1989@gmail.com>
2023-03-01 01:33:22 +0530

    Project init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './compnents/home/home.component';
+import { LoginComponent } from './compnents/login/login.component';
+import { AboutComponent } from './compnents/about/about.component';
+import { NavComponent } from './compnents/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    AboutComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
