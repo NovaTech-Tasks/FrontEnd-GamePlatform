40a42519fc7d756f8eb9a52e94784a7037a1795f
ThanuraSilva <thanurasilva1989@gmail.com>
2023-03-01 18:04:57 +0530

    Base64 data handling start

--- src/app/compnents/game-mgmt/game-mgmt.component.ts
+++ src/app/compnents/game-mgmt/game-mgmt.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {GamesService} from "../../service/games.service";
 import { FormGroup,FormControl} from "@angular/forms";
+import {observable, Observable, Subscriber} from "rxjs";
+import {error} from "@angular/compiler/src/util";
 
 @Component({
   selector: 'app-game-mgmt',
@@ -9,6 +11,7 @@ import { FormGroup,FormControl} from "@angular/forms";
 })
 export class GameMgmtComponent implements OnInit {
   gamesData:any;
+  gameImage:any;
   constructor(private gamesService:GamesService) { }
 
   ngOnInit(): void {
@@ -26,5 +29,34 @@ export class GameMgmtComponent implements OnInit {
      },(error) =>{
        console.log(error)
      })
+  }
+
+  onChange($event : Event) {
+    // @ts-ignore
+    const file = ($event.target as HTMLInputElement).files[0];
+    //console.log(file)
+    this.convertToBase64(file);
+  }
+  convertToBase64(file:File){
+    const obs = new Observable((subscriber:Subscriber<any>) =>{
+         this.readFile(file,subscriber)
+    });
+    obs.subscribe((data) =>{
+      console.log(data)
+      this.gameImage = data;
+    })
+  }
+
+  readFile(file:File,subscriber: Subscriber<any>) {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+    fileReader.onload = () =>{
+      subscriber.next(fileReader.result);
+      subscriber.complete();
+    };
+    fileReader.onerror = (error) =>{
+      subscriber.error(error);
+      subscriber.complete();
+    }
   }
 }
